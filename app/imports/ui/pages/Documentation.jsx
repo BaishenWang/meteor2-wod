02aab333d4cd709d52a6bd867a337c76e224c38d
Baishen Wang <89823629+BaishenWang@users.noreply.github.com>
2023-10-26 12:53:25 -1000

    push

---
+++ app/imports/ui/pages/Documentation.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Col, Container, Image, Row } from 'react-bootstrap';
+
+<p>First off, check the meteor-application-template-react github.io page.</p>
+<p>If that does not answer your question, you can post a smart question to the class Discord.</p>
+<p>If all else fails, post a question to the Meteor Forum site.</p>
